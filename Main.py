5caef07151477d6e9d08e7eb1e018329c53584bf
mehki woodard <woodameh000@warren.k12.in.us>
2024-11-18 10:18:57 -0500

    intial commit

---
+++ Main.py
@@ -0,0 +1,27 @@
+fruit_tuple = ("apple", "banana", "cherry", "date")
+print(fruit_tuple[0])
+my_tuple = ("apple", "banana", "cherry", "date")
+print(my_tuple[0])  # Output: 'a'
+
+print(my_tuple[-1])  # Output: 'd'
+
+my_tuple = (10, 20, 30, 40, 50)
+
+print(my_tuple[:3])  # Output: (apple, banana, cherry)
+
+print(my_tuple[1:])  # Output: (apple, banana, cherry, dates)
+
+print(my_tuple[::2])  # Output: (apple, cherry, dates)
+
+
+my_tuple = (1, 2, 2, 3, 4)
+
+print(my_tuple.count(2))  # Output: 2
+
+print(my_tuple.index(3))  # Output: 3
+slice_tuple = (10, 20, 30, 40, 50)
+
+
+num_tuple = (3, 5, 3, 2, 8, 3, 7)
+
+
